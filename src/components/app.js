99aa5a0b62b1d730f44ba7d16006cf80dae0fadd
mterrado <mcterrado@me.com>
2018-12-20 14:10:37 -0800

    React Functional Components

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,28 @@
 import React from 'react';
+import Table from './table';
 
 export default () => {
-    return <h1>My first React functional component</h1>
+    const students = [
+        {
+            name: 'Peter Parker',
+            course: 'Intro to Photography',
+            grade: 85
+        },
+        {
+            name: 'Bruce Banner',
+            course: 'Psychology',
+            grade: 85
+        },
+        {
+            name: 'Tony Stark',
+            course: 'Mechanical Engineering',
+            grade: 100
+        }
+    ];
+    return (
+        <div className="container">
+            <h1>Student Grade Table</h1>
+            <Table data={students}/>
+        </div>
+    )
 }
-
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './components/app';
 
 ReactDOM.render(
-    //<h1 className="container">React Prototypes!</h1>,
     <App/>,
     document.getElementById('root')
 );
