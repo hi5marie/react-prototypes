d0c9002b43dec0ce14e1b4e6aca3f594a5b082a5
mterrado <mcterrado@me.com>
2018-12-26 10:34:40 -0800

    feature set 5

---
+++ src/components/contact_form.js
@@ -0,0 +1,62 @@
+import React, {Component} from 'react';
+import Field from './field';
+
+
+class ContactForm extends Component{
+     constructor(props){
+         super(props);
+
+         this.state = {
+             form: {
+                 firstName: '',
+                 lastName: '',
+                 phone: '',
+                 email: ''
+             }
+
+         }
+
+     }
+
+     handleInputChange = (event) => {
+
+         const {value, name} = event.target;
+
+         const {form} = this.state;
+
+         form[name] = value;
+
+         this.setState({
+             form: {...form}
+         });
+     }
+
+     handleSubmit = (event) => {
+         event.preventDefault();
+         console.log('handleSubmit called. Form Values are:', this.state.form);
+     }
+
+
+    render(){
+         const {firstName, lastName, phone, email} = this.state.form;
+
+        return (
+            <form onSubmit={this.handleSubmit}>
+
+            <Field name="firstName" label="First Name" type="text" value={firstName} onChange={this.handleInputChange}/>
+            <Field name="lastName" label="Last Name" type="text" value={lastName} onChange={this.handleInputChange}/>
+            <Field name="phone" label="First Name" type="text" value={firstName} onChange={this.handleInputChange}/>
+            <Field name="firstName" label="First Name" type="text" value={firstName} onChange={this.handleInputChange}/>
+
+                <button>Add Contact</button>
+
+
+
+            </form>
+        )
+
+    }
+
+}
+
+export default ContactForm;
---
+++ src/components/field.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default props => {
+
+    console.log('Field props:', props);
+
+    return (
+
+        <div className="form-group">
+
+            <label>{props.label}</label>
+
+            <input {props.label} className="form-control"/>
+
+        </div>
+
+    )
+
+}
