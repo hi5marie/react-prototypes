32e788282b60aab974e1b2192aa4e83c9870ab72
mterrado <mcterrado@me.com>
2019-02-20 18:10:35 -0800

    Feature set 3 completed

--- src/components/movie_container.js
+++ src/components/movie_container.js
@@ -1,9 +1,31 @@
 import React, {Component} from 'react';
 import Movie from './movies';
+import axios from 'axios';
 
 class MoviesContainer extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            movies: []
+        }
+    }
+
+    componentDidMount(){
+        // const url = axios.get('http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/json').then((response)=>{ });
+
+        const url = 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/json';
+        axios.get(url).then( (resp)=>{
+            // console.log('Resp:', resp);
+
+            this.setState({
+                movies: resp.data.feed.entry
+            })
+        });
+    }
 
     render(){
+        console.log('render this.state:', this.state);
         return (
 
             <div>
