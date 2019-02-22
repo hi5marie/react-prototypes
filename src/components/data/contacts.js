6ae8d6c5bba7b6c9bdd1b19d138876eff317a6ff
mterrado <mcterrado@me.com>
2019-02-21 23:05:42 -0800

    Added the black canary contact

--- src/components/data/contacts.js
+++ src/components/data/contacts.js
@@ -7,18 +7,40 @@ export default [
         firstName: 'Clark',
         lastName: 'Kent',
         phone: '785-328-9020',
-        email: 'superman@google.com'
+        email: 'superman@google.com',
+        streetAddress: '344 Clinton Street',
+        city: 'Metropolis',
+        state: 'Kansas',
+        zipCode: '66102'
     },
     {
         firstName: 'Bruce',
         lastName: 'Wayne',
         phone: '201-725-5692',
-        email: 'theRealBatman@google.com'
+        email: 'theRealBatman@google.com',
+        streetAddress: '1007 Mountain Drive',
+        city: 'Gotham Drive',
+        state: 'New Jersey',
+        zipCode: '07097'
+
     },
     {
         firstName: 'Barry',
         lastName: 'Allen',
         phone: '319-210-7259',
-        email: 'theFlash@google.com'
+        email: 'theFlash@google.com',
+        streetAddress: '1522 Pleasant Rd',
+        city: 'Fallville',
+        state: 'Iowa',
+        zipCode: '50001'
+    },
+    {
+        firstName: 'Dinah',
+        lastName: 'Drake',
+        phone: '618-745-8982',
+        email: 'theBlackCanary@google.com',
+        city: 'Star City',
+        state: 'Illinois',
+        zipCode: '62047'
     }
 ]
